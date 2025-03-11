4a7d40bd703b0afbbf580cccce3691558fa4db41
lahcen404 <lahcen.maskour2003@gmail.com>
2025-03-11 14:42:21 +0000

    add Entraineur Model

--- src/main/java/Entraineur/Model/Entraineur.java
+++ src/main/java/Entraineur/Model/Entraineur.java
@@ -1,4 +1,46 @@
 package Entraineur.Model;
 
 public class Entraineur {
-}
+
+
+        private int id;
+        private String nom;
+        private String specialite;
+
+
+        public Entraineur() {
+        }
+
+
+        public Entraineur(int id, String nom, String specialite) {
+            this.id = id;
+            this.nom = nom;
+            this.specialite = specialite;
+        }
+
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public String getSpecialite() {
+            return specialite;
+        }
+
+        public void setSpecialite(String specialite) {
+            this.specialite = specialite;
+        }
+    }
+
