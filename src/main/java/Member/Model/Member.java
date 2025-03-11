4881b7b0a297d9567adf17f84dee6ac60f6274a0
lahcen404 <lahcen.maskour2003@gmail.com>
2025-03-11 14:40:21 +0000

    add Member Model

--- src/main/java/Member/Model/Member.java
+++ src/main/java/Member/Model/Member.java
@@ -1,6 +1,59 @@
 package Member.Model;
 
+
+import java.time.LocalDate;
+
 public class Member {
 
+        private int id;
+        private String nom;
+        private LocalDate dateNaissance;
+        private String sport;
+
+
+        public Member() {
+
+        }
+
+
+        public Member(int id, String nom, LocalDate dateNaissance, String sport) {
+            this.id = id;
+            this.nom = nom;
+            this.dateNaissance = dateNaissance;
+            this.sport = sport;
+        }
+
+        // Getters and Setters
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public LocalDate getDateNaissance() {
+            return dateNaissance;
+        }
+
+        public void setDateNaissance(LocalDate dateNaissance) {
+            this.dateNaissance = dateNaissance;
+        }
+
+        public String getSport() {
+            return sport;
+        }
+
+        public void setSport(String sport) {
+            this.sport = sport;
+        }
     }
 
