a8fd6832d7d1228c2c5e503cb9d3c60053447894
lahcen404 <lahcen.maskour2003@gmail.com>
2025-03-12 14:57:54 +0000

    create functions Add Member and Entraineur

--- src/main/java/Authentification/RegisterServlet.java
+++ src/main/java/Authentification/RegisterServlet.java
@@ -42,7 +42,6 @@ if(role.equals("member")){
     MemberDAO memberDAO = new MemberDAO();
 
     memberDAO.ajouterMember(member);
-
 }else if(role.equals("entraineur")){
 
     String username = req.getParameter("username");
