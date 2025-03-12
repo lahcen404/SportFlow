8ad4f6837348705333757fc89bb6f28031d330e2
lahcen404 <lahcen.maskour2003@gmail.com>
2025-03-12 23:44:28 +0000

    add SQL Script

--- src/main/resources/SQL Script SportFlow.sql
+++ src/main/resources/SQL Script SportFlow.sql
@@ -1,17 +1,26 @@
 CREATE DATABASE SportFlowDB;
 USE SportFlowDB;
 
+CREATE TABLE Admin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE Members (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
     dateNaissance DATE NOT NULL,
     sport VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Entraineurs (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
     specialite VARCHAR(50) NOT NULL
 );
 
@@ -24,23 +33,10 @@ CREATE TABLE Seances (
     FOREIGN KEY (entraineur_id) REFERENCES Entraineurs(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    role ENUM ('Admin', 'Member', 'Entraineur') ,
-    member_id INT,
-    entraineur_id INT,
-    FOREIGN KEY (member_id) REFERENCES Members(id) ON DELETE CASCADE,
-    FOREIGN KEY (entraineur_id) REFERENCES Entraineurs(id) ON DELETE CASCADE);
 
-
-
-
-SELECT * FROM Users;
+SELECT * FROM Admin;
 SELECT * FROM Members;
 SELECT * FROM Entraineurs;
 SELECT * FROM Seances;
 
- -- drop database sportflowdb;
 SHOW TABLES;
